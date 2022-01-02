c17e4132393106a94e139fddeb3a08f433c406be
Owilly <pasaclouma54@gmail.com>
2022-01-02 20:33:50 +0300

    add hover effect on all portfolio images

--- script.js
+++ script.js
@@ -13,13 +13,76 @@ $(document).ready(function(){
    });
 
    $('#firstProject').mouseenter(function(){
-       $('.work4').toggle();
        $('.hover1').toggle();
-   })
+       $('.work1').toggle();
+   });
    $('#firstProject').mouseleave(function(){
-    $('.work4').show();
-    $('.hover1').show();
-})
+    $('.hover1').toggle();
+    $('.work1').toggle();
+});
+
+$('#secondProject').mouseenter(function(){
+    $('.hover2').toggle();
+    $('.work2').toggle();
+});
+$('#secondProject').mouseleave(function(){
+ $('.hover2').toggle();
+ $('.work2').toggle();
+});
+
+$('#thirdProject').mouseenter(function(){
+    $('.hover3').toggle();
+    $('.work3').toggle();
+});
+$('#thirdProject').mouseleave(function(){
+ $('.hover3').toggle();
+ $('.work3').toggle();
+});
+$('#fourthProject').mouseenter(function(){
+    $('.hover4').toggle();
+    $('.work4').toggle();
+});
+$('#fourthProject').mouseleave(function(){
+ $('.hover4').toggle();
+ $('.work4').toggle();
+});
+
+$('#fifthProject').mouseenter(function(){
+    $('.hover5').toggle();
+    $('.work5').toggle();
+});
+$('#fifthProject').mouseleave(function(){
+ $('.hover5').toggle();
+ $('.work5').toggle();
+});
+
+$('#sixthProject').mouseenter(function(){
+    $('.hover6').toggle();
+    $('.work6').toggle();
+});
+$('#sixthProject').mouseleave(function(){
+ $('.hover6').toggle();
+ $('.work6').toggle();
+});
+
+$('#seventhProject').mouseenter(function(){
+    $('.hover7').toggle();
+    $('.work7').toggle();
+});
+$('#seventhProject').mouseleave(function(){
+ $('.hover7').toggle();
+ $('.work7').toggle();
+});
+
+$('#eighthProject').mouseenter(function(){
+    $('.hover8').toggle();
+    $('.work8').toggle();
+});
+$('#eighthProject').mouseleave(function(){
+ $('.hover8').toggle();
+ $('.work8').toggle();
+});
+
 
 
 });

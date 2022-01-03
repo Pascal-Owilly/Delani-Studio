8292d814a8f0d7a4d3bc7c1ccbc6d6a111d9117a
Owilly <pasaclouma54@gmail.com>
2022-01-03 08:06:31 +0300

    add flex to media queries to respond to mobile devices

--- script.js
+++ script.js
@@ -12,75 +12,92 @@ $(document).ready(function(){
        $('.showThirdText').toggle();
    });
 
-   $('#firstProject').mouseenter(function(){
-       $('.hover1').toggle();
-       $('.work1').toggle();
+   $('#project1').mouseenter(function(){
+       $('#projectOne').css({ 'background': 'rgba(38, 31, 15,0.5', 'border': '10px solid #fff', 'font-weight': '500', 'font-size': '20px' }) ;
+       $('#firstOverlay').toggle();
+       $('#projectOne').toggle();
    });
-   $('#firstProject').mouseleave(function(){
-    $('.hover1').toggle();
-    $('.work1').toggle();
+   $('#project1').mouseleave(function(){
+       $(this).css('background', 'transparent')
+    $('#firstOverlay').toggle(); 
+    $('#projectOne').toggle();
 });
 
-$('#secondProject').mouseenter(function(){
-    $('.hover2').toggle();
-    $('.work2').toggle();
+$('#project2').mouseenter(function(){
+    $('#projectTwo').css({ 'background': 'rgba(38, 31, 15,0.5', 'border': '10px solid #fff', 'font-weight': '500', 'font-size': '20px' }) ;
+    $('#secondOverlay').toggle();
+    $('#projectTwo').toggle();
 });
-$('#secondProject').mouseleave(function(){
- $('.hover2').toggle();
- $('.work2').toggle();
-});
-
-$('#thirdProject').mouseenter(function(){
-    $('.hover3').toggle();
-    $('.work3').toggle();
-});
-$('#thirdProject').mouseleave(function(){
- $('.hover3').toggle();
- $('.work3').toggle();
-});
-$('#fourthProject').mouseenter(function(){
-    $('.hover4').toggle();
-    $('.work4').toggle();
-});
-$('#fourthProject').mouseleave(function(){
- $('.hover4').toggle();
- $('.work4').toggle();
+$('#project2').mouseleave(function(){
+    $(this).css('background', 'transparent')
+ $('#secondOverlay').toggle(); 
+ $('#projectTwo').toggle();
 });
 
-$('#fifthProject').mouseenter(function(){
-    $('.hover5').toggle();
-    $('.work5').toggle();
+$('#project3').mouseenter(function(){
+    $('#projectThree').css({ 'background': 'rgba(38, 31, 15,0.5', 'border': '10px solid #fff', 'font-weight': '500', 'font-size': '20px' }) ;
+    $('#thirdOverlay').toggle();
+    $('#projectThree').toggle();
 });
-$('#fifthProject').mouseleave(function(){
- $('.hover5').toggle();
- $('.work5').toggle();
-});
-
-$('#sixthProject').mouseenter(function(){
-    $('.hover6').toggle();
-    $('.work6').toggle();
-});
-$('#sixthProject').mouseleave(function(){
- $('.hover6').toggle();
- $('.work6').toggle();
+$('#project3').mouseleave(function(){
+    $(this).css('background', 'transparent')
+ $('#thirdtOverlay').toggle(); 
+ $('#projectThree').toggle();
 });
 
-$('#seventhProject').mouseenter(function(){
-    $('.hover7').toggle();
-    $('.work7').toggle();
+$('#project4').mouseenter(function(){
+    $('#projectFour').css({ 'background': 'rgba(38, 31, 15,0.5', 'border': '10px solid #fff', 'font-weight': '500', 'font-size': '20px' }) ;
+    $('#fourthOverlay').toggle();
+    $('#projectFour').toggle();
 });
-$('#seventhProject').mouseleave(function(){
- $('.hover7').toggle();
- $('.work7').toggle();
+$('#project4').mouseleave(function(){
+    $(this).css('background', 'transparent')
+ $('#fourthOverlay').toggle(); 
+ $('#projectFour').toggle();
 });
 
-$('#eighthProject').mouseenter(function(){
-    $('.hover8').toggle();
-    $('.work8').toggle();
+$('#project5').mouseenter(function(){
+    $('#projectFive').css({ 'background': 'rgba(38, 31, 15,0.5', 'border': '10px solid #fff', 'font-weight': '500', 'font-size': '20px' }) ;
+    $('#fifthOverlay').toggle();
+    $('#projectFive').toggle();
 });
-$('#eighthProject').mouseleave(function(){
- $('.hover8').toggle();
- $('.work8').toggle();
+$('#project5').mouseleave(function(){
+    $(this).css('background', 'transparent')
+ $('#fifthtOverlay').toggle(); 
+ $('#projectFive').toggle();
+});
+
+$('#project6').mouseenter(function(){
+    $('#projectSix').css({ 'background': 'rgba(38, 31, 15,0.5', 'border': '10px solid #fff', 'font-weight': '500', 'font-size': '20px' }) ;
+    $('#SixthOverlay').toggle();
+    $('#projectSix').toggle();
+});
+$('#project6').mouseleave(function(){
+    $(this).css('background', 'transparent')
+ $('#sixthOverlay').toggle(); 
+ $('#projectSix').toggle();
+});
+
+$('#project7').mouseenter(function(){
+    $('#projectSeven').css({ 'background': 'rgba(38, 31, 15,0.5', 'border': '10px solid #fff', 'font-weight': '500', 'font-size': '20px' }) ;
+    $('#SeventhOverlay').toggle();
+    $('#projectSeven').toggle();
+});
+$('#project7').mouseleave(function(){
+    $(this).css('background', 'transparent')
+ $('#seventhOverlay').toggle(); 
+ $('#projectSeven').toggle();
+});
+
+$('#project8').mouseenter(function(){
+    $('#projectEight').css({ 'background': 'rgba(38, 31, 15,0.5', 'border': '10px solid #fff', 'font-weight': '500', 'font-size': '20px' }) ;
+    $('#eighthOverlay').toggle();
+    $('#projectEight').toggle();
+});
+$('#project8').mouseleave(function(){
+    $(this).css('background', 'transparent')
+ $('#eighthOverlay').toggle(); 
+ $('#projectEight').toggle();
 });
 
 
